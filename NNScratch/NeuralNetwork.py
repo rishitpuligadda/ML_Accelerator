dd4c503c8fcdaf34e61a270e33ffdc900df8a12c
rishitpuligadda <puligaddarishit@gmail.com>
2025-09-05 20:22:32 +0530

    categorical cross entropy class is added

--- NNScratch/NeuralNetwork.py
+++ NNScratch/NeuralNetwork.py
@@ -39,6 +39,18 @@ class Loss:
         data_loss = np.mean(sample_losses)
         return data_loss
 
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if (len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif (len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        Totloss = -np.log(correct_confidences)
+        return Totloss
+
 layer1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 layer2 = Layer_Dense(3, 3)
