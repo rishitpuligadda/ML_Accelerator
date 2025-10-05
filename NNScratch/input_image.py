5631a2ab7bd47dc4f89590908fa5609b5455bd27
rishitpuligadda <puligaddarishit@gmail.com>
2025-10-05 17:40:26 +0530

    Data processing of an image so that it can be used by the system verilog code.

---
+++ NNScratch/input_image.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+# ---- Load and preprocess the image ----
+image_data = cv2.imread('../bag.png', cv2.IMREAD_GRAYSCALE)
+image_data = cv2.resize(image_data, (28, 28))
+image_data = 255 - image_data  # invert colors if needed
+image_data = (image_data.reshape(1, -1).astype(np.float32) - 127.5) / 127.5  # normalize [-1,1]
+
+# ---- Save as floating-point text ----
+np.savetxt('../parameters/input_image.txt', image_data, fmt="%.6f")
+
+print("input_image.txt generated successfully!")
+
